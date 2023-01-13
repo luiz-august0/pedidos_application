238d29f7a10ed7a2a919a8bd0607810e508f26fc
luiz-august0 <66564172+luiz-august0@users.noreply.github.com>
2023-01-13 14:06:50 -0300

    Atualizando versão do mysql

--- api/mysql/mysql.js
+++ api/mysql/mysql.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+import * as mysql from 'mysql2';
 require('dotenv').config();
 
 var pool = mysql.createPool({
@@ -6,7 +6,7 @@ var pool = mysql.createPool({
     "password" : process.env.PASSWORD,
     "database" : process.env.DATABASE,
     "host" : process.env.HOST,
-    "port" : process.env.PORT
+    "port" : process.env.PORTBASE
 })
 
 exports.pool = pool;
